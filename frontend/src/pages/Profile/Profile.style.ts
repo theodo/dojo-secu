96b23e996d6faff19e7557c2e511210163748873
Guillaume Cornet <guillaumec@theodo.fr>
2019-05-10 13:49:46 +0200

    Add blank page to front and redirection after login

---
+++ frontend/src/pages/Profile/Profile.style.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const StyledProfile = styled.div`
+  display: flex;
+`;
+
+export default StyledProfile;
--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router';
 const Home = lazy(() => import('./pages/Home'));
 const Login = lazy(() => import('./pages/Login'));
 const Avatar = lazy(() => import('./pages/Avatar'));
+const Profile = lazy(() => import('./pages/Profile'));
 
 const routes = () => (
   <Suspense fallback={<div>Loading...</div>}>
@@ -11,6 +12,7 @@ const routes = () => (
       <Route exact path="/" component={Home} />
       <Route path="/login" component={Login} />
       <Route path="/avatar" component={Avatar} />
+      <Route path="/profile" component={Profile} />
     </Switch>
   </Suspense>
 );
