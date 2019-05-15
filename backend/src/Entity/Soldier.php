bd2c282ef5b0da12ba910464db837191b08ab890
Guillaume Cornet <guillaumec@theodo.fr>
2019-05-15 13:51:05 +0200

    Add fake army table for level 3 and 4

---
+++ backend/src/Entity/Soldier.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+/**
+ * @ApiResource
+ * @ORM\Table(name="army")
+ * @ORM\Entity(repositoryClass="App\Repository\SoldierRepository")
+ */
+class Soldier
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     * @Groups({"soldiers_read"})
+     */
+    private $id;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string")
+     * @Groups({"soldiers_read"})
+     */
+    private $firstName;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string")
+     * @Groups({"soldiers_read"})
+     */
+    private $lastName;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string")
+     * @Groups({"soldiers_read"})
+     */
+    private $rank;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string")
+     */
+    private $password;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+        return $this;
+    }
+
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
+        return $this;
+    }
+
+    public function getRank(): string
+    {
+        return $this->rank;
+    }
+
+    public function setRank(string $rank): self
+    {
+        $this->rank = $rank;
+        return $this;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+        return $this;
+    }
+}
---
+++ backend/src/Migrations/Version20190515114936.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190515114936 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SEQUENCE army_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE army (id INT NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, rank VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('ALTER INDEX uniq_c2502824f85e0677 RENAME TO UNIQ_C2502824E7927C74');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP SEQUENCE army_id_seq CASCADE');
+        $this->addSql('DROP TABLE army');
+        $this->addSql('ALTER INDEX uniq_c2502824e7927c74 RENAME TO uniq_c2502824f85e0677');
+    }
+}
