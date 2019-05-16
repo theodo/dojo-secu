089385fe736425116b393dc9853f0189365ca05f
Guillaume Cornet <guillaumec@theodo.fr>
2019-05-16 14:26:40 +0200

    Change password to access code, add new fake corporals to table

--- backend/src/Migrations/Version20190516122444.php
+++ backend/src/Migrations/Version20190516122444.php
@@ -8,7 +8,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190515114936 extends AbstractMigration
+final class Version20190516122444 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
@@ -16,7 +16,7 @@ final class Version20190515114936 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
         $this->addSql('CREATE SEQUENCE army_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE army (id INT NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, rank VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE army (id INT NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, rank VARCHAR(255) NOT NULL, access_code VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('ALTER INDEX uniq_c2502824f85e0677 RENAME TO UNIQ_C2502824E7927C74');
     }
 
